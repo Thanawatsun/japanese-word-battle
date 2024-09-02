23366698e0d24958ac85abc53b57ab6d930332a0
Thanawat sritho <88428206+Thanawatsun@users.noreply.github.com>
2024-09-02 13:23:08 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import TermUploadForm from './page/test01';
+
 import reportWebVitals from './reportWebVitals';
 import WordBank from "./page/wordbank";
 const root = ReactDOM.createRoot(document.getElementById('root'));
