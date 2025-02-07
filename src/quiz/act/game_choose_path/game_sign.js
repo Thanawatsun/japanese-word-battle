9668d428ad3ffbef5f89fc2e544f333a3682d0e8
SSProgamer <72447877+SSProgamer@users.noreply.github.com>
2025-02-07 15:16:51 +0700

    Game choice sign done.

--- src/quiz/act/game_choose_path/game_sign.js
+++ src/quiz/act/game_choose_path/game_sign.js
@@ -7,13 +7,26 @@ function AnswerButton({ text, onClick, index }) {
     "https://firebasestorage.googleapis.com/v0/b/japanese-word-battle.appspot.com/o/img%2Fgame_assets%2Fsign%2Fsign_bus_right.png?alt=media&token=f31382a8-5410-4d28-aebb-c9dcff7f23ff",
     "https://firebasestorage.googleapis.com/v0/b/japanese-word-battle.appspot.com/o/img%2Fgame_assets%2Fsign%2Fsign_bus_left.png?alt=media&token=2a99ba40-f6fe-47c2-9bad-d1a9a2a79bd2",
   ];
+  const signHoverBGImages = [
+    "https://firebasestorage.googleapis.com/v0/b/japanese-word-battle.appspot.com/o/img%2Fgame_assets%2Fsign%2Fsign_hover_train_right.png?alt=media&token=22f789d0-1896-4cad-b93c-cf155cf4e454",
+    "https://firebasestorage.googleapis.com/v0/b/japanese-word-battle.appspot.com/o/img%2Fgame_assets%2Fsign%2Fsign_hover_train_left.png?alt=media&token=6e83fe85-a95d-40b1-8d75-e5c55cb8050d",
+    "https://firebasestorage.googleapis.com/v0/b/japanese-word-battle.appspot.com/o/img%2Fgame_assets%2Fsign%2Fsign_hover_bus_right.png?alt=media&token=37f1ccbd-8683-4346-b2da-199207e906cf",
+    "https://firebasestorage.googleapis.com/v0/b/japanese-word-battle.appspot.com/o/img%2Fgame_assets%2Fsign%2Fsign_hover_bus_left.png?alt=media&token=3ba90075-16d8-4aff-a79a-1588e623acbb",
+  ];
+  const [isHovered, setIsHovered] = useState(false);
 
   return (
     <button
       onClick={onClick}
       className="sign-button"
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
       style={{
-        backgroundImage: `url(${signBGImages[index % signBGImages.length]})`,
+        backgroundImage: `url(${
+          isHovered
+            ? signHoverBGImages[index % signHoverBGImages.length]
+            : signBGImages[index % signBGImages.length]
+        })`,
         backgroundSize: "cover",
         backgroundPosition: "center",
       }}
@@ -49,21 +62,27 @@ function Game_sign({ game_data, setload_act }) {
     <div>
       <div className="choice-background-image-box">
         <img
-          src="https://firebasestorage.googleapis.com/v0/b/japanese-word-battle.appspot.com/o/img%2Fgame_assets%2Fgame_path_1.png?alt=media&token=959d7e51-bc6d-4e00-94bb-e528708024a4"
+          src="https://firebasestorage.googleapis.com/v0/b/japanese-word-battle.appspot.com/o/img%2Fgame_assets%2Fgame_path_1.png?alt=media&token=a8bf25ff-c004-4bd0-ad01-d92b0d37f595"
           alt="Sign-Background-Image"
           className="choice-background-image"
         />
       </div>
       <div className="choice-box">
-        <div>Choose the correct way to {game_data.wantToGo}.</div>
-        {game_data.options.map((choose, index) => (
-          <AnswerButton
-            key={choose.id}
-            text={choose.text}
-            onClick={() => handleClick(choose)}
-            index={index}
-          />
-        ))}
+        <div className="sign-question">
+          <h3 className="sign-question-text">
+            Choose the correct way to {game_data.wantToGo}.
+          </h3>
+        </div>
+        <div className="sign-box">
+          {game_data.options.map((choose, index) => (
+            <AnswerButton
+              key={choose.id}
+              text={choose.text}
+              onClick={() => handleClick(choose)}
+              index={index}
+            />
+          ))}
+        </div>
       </div>
       {showBar && (
         <div className="green-con">
